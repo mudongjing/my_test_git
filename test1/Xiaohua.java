b39f9b167db8d3ddbf9a89cf02a1791824d95b06
eulernew@163.com <35022560+mudongjing@users.noreply.github.com>
2021-04-19 16:26:56 +0800

    test1

---
+++ test1/Xiaohua.java
@@ -0,0 +1,21 @@
+package org.my_test;
+
+public class Xiaohua {
+    private String username;
+    private String email;
+    private String school;
+    private String specialty;
+    public String getUsername(){
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getSchool(){
+        return school;
+    }
+    public String getSpecialty(){
+        return specialty;
+    }
+
+}
